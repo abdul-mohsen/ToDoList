70011a5b37c9a74e5596530c602cf3c368abc4e6
Mohsen(ssda) <abdulmohsen.almajed@tuwaiq.edu.sa>
2020-11-02 21:38:40 +0300

    Added the Done and discrad icon and created a menu xml for the add task fragment xml

--- app/src/main/java/com/bignerdranch/android/todolist/AddTask.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/AddTask.kt
@@ -1,19 +1,28 @@
 package com.bignerdranch.android.todolist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class AddTask:Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.title = null
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         val view = inflater.inflate(R.layout.fragment_add_task, container, false)
-
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_add_task, menu)
     }
 }
