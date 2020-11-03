394fd267eb8480cccd476d6a332659529844c41f
Mohsen(ssda) <abdulmohsen.almajed@tuwaiq.edu.sa>
2020-11-03 11:52:50 +0300

    Added the Dao class with all query needed

---
+++ app/src/main/java/com/bignerdranch/android/todolist/database/TaskDao.kt
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.todolist.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.bignerdranch.android.todolist.Task
+import java.util.*
+
+@Dao
+interface TaskDao {
+
+    @Query("SELECT * FROM task")
+    fun getTasks(): LiveData<List<Task>>
+
+    @Query("SELECT * FROM task WHERE id=(:id)")
+    fun getTask(id: UUID): LiveData<Task>
+
+    @Update
+    fun updateTasks(task:Task)
+
+    @Insert
+    fun insertTasks(task:Task)
+
+}
